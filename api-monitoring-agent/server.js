d2d286f8cfb51ccee6fb2cbaec4021e9dafd8aea
Gayathri2016 <gayathri_m@live.com>
2025-03-10 20:48:48 -0700

    added docker files

--- api-monitoring-agent/server.js
+++ api-monitoring-agent/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const ApiLog = require("./models/ApiLog"); // Import the model
 
 const app = express();
-const PORT = process.env.PORT || 5010;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
